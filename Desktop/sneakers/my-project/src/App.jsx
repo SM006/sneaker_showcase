2e0c919587127f9642bad632d36e3286a2776d5b
SM006 <shreyam1005@gmail.com>
2024-09-29 17:12:14 +0530

    fixed typo

---
+++ Desktop/sneakers/my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/hero';
+
+
+
+const App = () => {
+  return (
+    <main className='overflow-x-hidden'>
+      <Hero />
+    </main>
+  );
+}
+
+export default App
